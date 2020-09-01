7f0eeb5a4cc25dd7d7c96e7bf1f4640c4bf50ef1
tpups <jghtch@gmail.com>
2020-08-31 20:33:46 -0700

    modularize

---
+++ get_live_standings.py
@@ -0,0 +1,40 @@
+import json
+import requests
+import datetime
+import pymongo
+import get_standings
+
+wfbcLiveStats = "https://www.rotowire.com/mlbcommish20/tables/standings-live.php?leagueID=163&type=S&divisionID=0"
+wfbcLiveRanks = "https://www.rotowire.com/mlbcommish20/tables/standings-live.php?leagueID=163&type=R&divisionID=0"
+
+standings = get_standings.getStandings(wfbcLiveStats)
+standings = standings[0]
+
+print(type(standings))
+
+if standings is not None:
+    print("Standings:")
+    # for team in standings:
+    #     print(team)
+    print("teamname : " + standings["teamname"])
+    print("teamID : " + standings["teamID"])
+    print("owner : " + standings["owner"])
+    print("cat_4 (HR) : " + str(standings["cat_4"]))
+    print("cat_5 (RBI) : " + str(standings["cat_5"]))
+    print("cat_6 (SB) : " + str(standings["cat_6"]))
+    print("cat_13 (R) : " + str(standings["cat_13"]))
+    print("cat_16 (AVG) : " + str(standings["cat_16"]))
+    print("cat_21 (OPS) : " + str(standings["cat_21"]))
+    print("cat_30 (SV) : " + str(standings["cat_30"]))
+    print("cat_32 (IP) : " + str(standings["cat_32"]))
+    print("cat_33 (K) : " + str(standings["cat_33"]))
+    print("cat_45 (WHIP) : " + str(standings["cat_45"]))
+    print("cat_46 (ERA) : " + str(standings["cat_46"]))
+    print("cat_86 (QS) : " + str(standings["cat_86"]))
+    print("TOT : " + str(standings["TOT"]))
+
+
+
+from pymongo import MongoClient
+client = MongoClient()
+db = client.wfbc
--- get_standings.py
+++ get_standings.py
@@ -1,25 +1,5 @@
 import json
 import requests
-import datetime
-import pymongo
-
-datetime_obj = datetime.datetime.now()
-time_delta_1day = datetime.timedelta(1)
-datetime_obj = datetime_obj - time_delta_1day
-year = str(datetime_obj.year)
-month = str(datetime_obj.month)
-day = str(datetime_obj.day)
-if len(month) is 1:
-    month = "0" + month
-if len(day) is 1:
-    day = "0" + day
-end_date = year + "-" + month + "-" + day
-
-wfbcStandingsByDate = 'https://www.rotowire.com/mlbcommish20/tables/standings-by-date.php?leagueID=163&divisionID=0&start=2020-07-23&end=' + end_date
-wfbcLiveStats = "https://www.rotowire.com/mlbcommish20/tables/standings-live.php?leagueID=163&type=S&divisionID=0"
-wfbcLiveRanks = "https://www.rotowire.com/mlbcommish20/tables/standings-live.php?leagueID=163&type=R&divisionID=0"
-
-print(wfbcStandingsByDate)
 
 headers = {
     'Host': 'www.rotowire.com',
@@ -38,44 +18,12 @@ headers = {
     'Cookie': '*****'
 }
 
-def getStandings():
-    response = requests.get(wfbcStandingsByDate, headers=headers)
+def getStandings(url):
+    response = requests.get(url, headers=headers)
     print(response.status_code)
     if response.status_code == 200:
         return response.json()
     else:
         return None
-
-standings = getStandings()
-standings = standings[0]
-
-print(type(standings))
-
-if standings is not None:
-    print("Standings:")
-    # for team in standings:
-    #     print(team)
-    print("teamname : " + standings["teamname"])
-    print("teamID : " + standings["teamID"])
-    print("owner : " + standings["owner"])
-    print("cat_4 (HR) : " + standings["cat_4"])
-    print("cat_5 (RBI) : " + standings["cat_5"])
-    print("cat_6 (SB) : " + standings["cat_6"])
-    print("cat_13 (R) : " + standings["cat_13"])
-    print("cat_16 (AVG) : " + standings["cat_16"])
-    print("cat_21 (OPS) : " + standings["cat_21"])
-    print("cat_30 (SV) : " + standings["cat_30"])
-    print("cat_32 (IP) : " + standings["cat_32"])
-    print("cat_33 (K) : " + standings["cat_33"])
-    print("cat_45 (WHIP) : " + standings["cat_45"])
-    print("cat_46 (ERA) : " + standings["cat_46"])
-    print("cat_86 (QS) : " + standings["cat_86"])
-    print("TOT : " + standings["TOT"])
-
-
-
-from pymongo import MongoClient
-client = MongoClient()
-db = client.wfbc
 
 
