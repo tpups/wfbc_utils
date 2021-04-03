a35ef4779acc24a3de8b72be35051ac66b039fa9
tpups <jghtch@gmail.com>
2021-04-02 18:45:22 -0700

    2021 season dates

--- inputs.py
+++ inputs.py
@@ -4,14 +4,22 @@ import arrow
 
 # 2020 season start = July 23
 # 2020 season end = September 27
+# season_start = date(2020, 7, 23)
+# season_end = date(2020, 9, 27)
+
+# 2021 season start = April 1
+# 2021 season end = October 3
 season_start = date(2020, 7, 23)
 season_end = date(2020, 9, 27)
+
 # DATES AND TIMES
 utcnow = arrow.utcnow()
 pstnow = utcnow.to('US/Pacific')
 today = pstnow.date()
+
 # League Size
 numTeams = 12
+
 # mongo stuff
 client = MongoClient()
 db = client.wfbc2020
