ce304d71466777f4be8fca133d314093b102f66c
Josh Hutchinson <jghtch@gmail.com>
2023-12-17 15:07:59 -0800

    add rosters.csv to gitignore

--- main.py
+++ main.py
@@ -14,15 +14,32 @@ from inputs import utcnow, pstnow, client
 from BuildStandings import buildStandings
 from GetRosters import getAllRosters
 
+def UpdateDB(box, year):
+    if box is not False:
+        # pass in true updates league box, false for team box
+        for team in box:
+            updateResult = updateBox(year, team, False)
+            if updateResult['newEntry'] is True:
+                print('New box scores were added')
+            if updateResult['updateExisting'] is True:
+                print('Existing box scores were updated')
+            elif updateResult['newEntry'] is False and updateResult['updateExisting'] is False:
+                print('No box scores to update')
 
 # create timezone-aware(double check this) date to pass in:
 # dz = arrow.get(datetime(2020, 9, 24), 'US/Pacific').date()
 
 # file = open('2023.json')
 # box = json.load(file)
+                
+box = getAllLeagueBoxScores("2021")
+UpdateDB(box, "2021")
+# box = getAllLeagueBoxScores("2020")
+# UpdateDB(box, "2020")
+# box = getAllLeagueBoxScores("2019")
+# UpdateDB(box, "2019")
 
 # CREATE BOX SCORE JSON
-box = getAllLeagueBoxScores("2023")
 # json_box = json.dumps(box)
 # with open("2023.json", "w") as outfile:
 #     outfile.write(json_box)
@@ -33,17 +50,6 @@ box = getAllLeagueBoxScores("2023")
 # keep_col = ['manager', 'player', 'team']
 # new_f = f[keep_col]
 # new_f.to_csv("rosters.csv", index=False)
-
-if box is not False:
-    # pass in true updates league box, false for team box
-    for team in box:
-        updateResult = updateBox("2023", team, False)
-    if updateResult['newEntry'] is True:
-        print('New box scores were added')
-    if updateResult['updateExisting'] is True:
-        print('Existing box scores were updated')
-    elif updateResult['newEntry'] is False and updateResult['updateExisting'] is False:
-        print('No box scores to update')
 
 # # league stats
 # hittingBox = db.league_box_hitting
